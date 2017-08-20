4c6ebd1c1372d1b56ee2a95bed132a589d2f63c5
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:26:17 +0530

    idk whats happening

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
-<div class="container"><a href="http://hyperx1.me" style="text-align:center !important;">GO BACK TO MAIN WEBSITE</a></div>
+<div class="container"><a href="http://hyperx1.me" style="text-align:center !important;margin: auto;width: 50%;">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
