713d24acabe15920fb9dd9e55625aeecb8c51a28
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:30:53 +0530

    dont underline

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
-<div style="text-align: center !important;" ><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
+<div style="text-align: center !important; text-decoration:none;" ><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
