d4cfe22cee492905e0985e7fc843e0f68de2eba7
The Demon <kartikbhalla12@gmail.com>
2017-08-20 22:49:05 +0530

    make it red

--- resources/themes/bootstrap/index.php
+++ resources/themes/bootstrap/index.php
@@ -36,7 +36,7 @@
                 <?php $breadcrumbs = $lister->listBreadcrumbs(); ?>
 
                 <p class="navbar-text">
-                    HyperX Downloads-<?php foreach($breadcrumbs as $breadcrumb): ?>
+                    <span style="color:red;">HyperX</span> Downloads-<?php foreach($breadcrumbs as $breadcrumb): ?>
                         <?php if ($breadcrumb != end($breadcrumbs)): ?>
                                 <a href="<?php echo $breadcrumb['link']; ?>"><?php echo $breadcrumb['text']; ?></a>
                                 <span class="divider">/</span>
