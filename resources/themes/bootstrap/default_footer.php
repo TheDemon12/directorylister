5b8c6cbea3a474096da42656ee336e91a5d756bb
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:28:03 +0530

    move style to div

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
-<div class="container"><a href="http://hyperx1.me" style="text-align:center !important;margin: auto;width: 50%;">GO BACK TO MAIN WEBSITE</a></div>
+<div style="margin: auto;width: 50%;padding-top:10px;" ><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
