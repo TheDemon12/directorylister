89114d744fa9b8f002c6b4e90c3a2efc2583fedd
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:18:50 +0530

    eh

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,5 +1,5 @@
-<a href="http://hyperx1.me"Go Back to main website</a>
+<a href="http://hyperx1.me">Go Back to main website</a>
 <br>
 <div class="footer">
-    Copyright © 2017 HyperX. All Rights Reserved.</a>
+    Copyright © 2017 HyperX. All Rights Reserved.
 </div>
