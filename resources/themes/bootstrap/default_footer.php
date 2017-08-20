c23a4eac4d20ea9336042e83768dd2100bd1c7a4
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:35:17 +0530

    add class to div

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,13 +1,14 @@
 <style>
 	a:hover { 
     text-decoration:none;
+	color:#fff;
 	}
 
 	#main {
 	text-align: center !important;
 	}
 </style>
-<div><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
+<div class="main"><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
