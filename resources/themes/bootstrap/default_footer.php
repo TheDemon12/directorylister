4f68213ae01499009d373e57bfd6a1d10d7e8dd9
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:22:50 +0530

    remove margin

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
-<a href="http://hyperx1.me" style="margin:0 auto 0 auto;text-align: center;">GO BACK TO MAIN WEBSITE</a>
+<a href="http://hyperx1.me" style="text-align: center;">GO BACK TO MAIN WEBSITE</a>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
