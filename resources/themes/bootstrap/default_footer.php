851608ecdca44cbe5de4882200db8c7be19f9ff5
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:24:10 +0530

    add in container

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
-<a href="http://hyperx1.me" style="text-align:center !important;">GO BACK TO MAIN WEBSITE</a>
+<div class="container"><a href="http://hyperx1.me" style="text-align:center !important;">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
