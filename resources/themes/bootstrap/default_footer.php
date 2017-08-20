90ef57f3f1b7d729c9c66939de4f7bd99c268b24
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:28:34 +0530

    remove width

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,4 +1,4 @@
-<div style="margin: auto;width: 50%;padding-top:10px;" ><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
+<div style="margin: auto;padding-top:10px;" ><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
