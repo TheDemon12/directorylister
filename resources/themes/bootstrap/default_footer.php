5237605e1972d030831ffedbfefbab954da75a19
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:36:12 +0530

    that was supposed to be id not class

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -8,7 +8,7 @@
 	text-align: center !important;
 	}
 </style>
-<div class="main"><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
+<div id="main"><a href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
