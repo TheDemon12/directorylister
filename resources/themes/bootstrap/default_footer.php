39522f37a3c3088f1b3dcc904448679310d30222
The Demon <kartikbhalla12@gmail.com>
2017-08-20 23:39:17 +0530

    gotcha

--- resources/themes/bootstrap/default_footer.php
+++ resources/themes/bootstrap/default_footer.php
@@ -1,5 +1,5 @@
 <style>
-	#foot :hover { 
+	a.foot:hover { 
     text-decoration:none;
 	color:#fff;
 	}
@@ -9,7 +9,7 @@
 	font-weight: bold;
 	}
 </style>
-<div id="main"><a id="foot" href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
+<div id="main"><a class="foot" href="http://hyperx1.me">GO BACK TO MAIN WEBSITE</a></div>
 <br>
 <div class="footer">
     Copyright © 2017 HyperX. All Rights Reserved.
